14aa8348a0dd3836c02a2d6a66174dd8c15f683f
Andrew Schreiber <andrew.schreiber@owllabs.com>
2019-11-11 19:59:33 -0700

    Mobile rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import React, { useState } from 'react';
 import { useSpring, animated, config } from 'react-spring'
+import { Breakpoint, BreakpointProvider } from 'react-socks'
 
 
 import './App.css';
 import { Map } from './components/Map';
 import { Colorado } from './data/colorado.geojson';
 import { Returns as ElectionResults } from './data/returns.json'
-import {Returns} from './components/Returns'
+import { Returns } from './components/Returns'
 
 const App: React.FC = () => {
   const [isOpen, setOpen] = useState(false)
   const animSpring = useSpring({
     transform: isOpen ? 'rotateY(-45deg) translateX(100px)' : 'rotateY(-0deg) translateX(0px)'
-   })
+  })
   return (
     <div className="App">
-      {/* 
+      <BreakpointProvider>
+        {/* 
         viewBox allows for the internal points of  the SVG to  always 
         be 960 units wide and 500 tall regardless of the size
 
@@ -23,10 +25,16 @@ const App: React.FC = () => {
         aspect ratio is maintained. 
 
        */}
-      <animated.svg style={animSpring} preserveAspectRatio="xMidYMid" viewBox="0 0 960 500">
-        <Map geoJSON={Colorado.features} results={ElectionResults}/>
-      </animated.svg>
-      <Returns isOpen={isOpen} setOpen={setOpen} results={ElectionResults}></Returns>
+        <Breakpoint xlarge up>
+          <animated.svg style={animSpring} preserveAspectRatio="xMidYMid" viewBox="0 0 960 500">
+            <Map geoJSON={Colorado.features} results={ElectionResults} />
+          </animated.svg>
+          <Returns isOpen={isOpen} setOpen={setOpen} results={ElectionResults}></Returns>
+        </Breakpoint>
+        <Breakpoint large down>
+          <Returns isOpen={true} setOpen={() => { }} results={ElectionResults} isAnimated={false}></Returns>
+        </Breakpoint>
+      </BreakpointProvider>
     </div>
   );
 }
--- src/components/Returns.tsx
+++ src/components/Returns.tsx
@@ -6,10 +6,11 @@ import { Return } from './Return'
 interface ReturnsProps {
     results: any,
     isOpen: boolean,
-    setOpen: Function
+    setOpen: Function,
+    isAnimated?: boolean
 }
 
-export const Returns: React.FC<ReturnsProps> = ({results, isOpen, setOpen}) => {
+export const Returns: React.FC<ReturnsProps> = ({results, isOpen, setOpen, isAnimated=true}) => {
    
    const animSpring = useSpring({
     width: isOpen ? '40%' : '5vw'
@@ -22,7 +23,7 @@ export const Returns: React.FC<ReturnsProps> = ({results, isOpen, setOpen}) => {
 
     const countyNames = Object.keys(results)
     return (
-        <animated.div className='results-pane' style={animSpring} onClick={() => setOpen(!isOpen)}>
+        <animated.div className='results-pane' style={isAnimated?animSpring:{}} onClick={() => setOpen(!isOpen)}>
             <animated.h2 style={textAnim}>Results</animated.h2>
             {countyNames.map((item, idx) => {
                 return <div key={idx}>
